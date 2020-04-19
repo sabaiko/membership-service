1266e9dcc1f7a7485495dd9801deba3fe386555a
Amrit Khadka <kha.amrit@gmail.com>
2020-04-19 17:04:04 -0400

    add a golang hello world and buildspec

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+  fmt.Println("hello world from golang")
+  fmt.Println("1+1 =", 1+1)
+  fmt.Println("7.0/3.0 =", 7.0/3.0)
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+  fmt.Println("good bye from golang")
+}
